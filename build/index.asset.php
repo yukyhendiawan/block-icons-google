537b4a08d62c5ef3397891d17037d3bc2aebb51f
yukyhendiawan <yukyhendiawan123@gmail.com>
2024-02-25 14:23:54 +0700

    refactor: remove gulp and replace with webpack

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'bbc64af92e53ee8c80e1');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'd2d65332a8a08f665dac');
